1fbaf66b6abf3869cfe3364ccc81f8a13fd4e60a
Danait378 <danfkr143@gmail.com>
2026-04-14 17:43:39 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,10 @@
 /*
 The sakila data base appears to be used for a movie rental system
 */
+
+USE Sakila;
+
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
+
