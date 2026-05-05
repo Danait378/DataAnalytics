bfbefb315150e955a736541d4a34163a399ea8b4
Danait378 <danfkr143@gmail.com>
2026-05-04 17:26:21 -0700

    updated exercise messages

---
+++ week-05/Ex1A_hello.py
@@ -0,0 +1,22 @@
+print("Hello world!")
+
+message = 'Hello world!' 
+print(message)
+
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = 0.50
+
+print(dollars + cents)
+
+#  Python adds the numbers together and shows the total as 3.5
+
+cents = cents + 0.25
+print(dollars + cents)
+
+d_str = "3 dollars"
+c_str = "50 cents"
+
+print(d_str, c_str)
