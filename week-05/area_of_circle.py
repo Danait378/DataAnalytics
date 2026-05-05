1b8f4b0fcb8fd84b95425057b33f047b3718b52c
Danait378 <danfkr143@gmail.com>
2026-05-05 15:29:02 -0700

    area_of_circle.py

---
+++ week-05/area_of_circle.py
@@ -0,0 +1,11 @@
+# diameter of circle
+diameter = 20
+
+# radius is half of diameter
+radius = diameter / 2
+
+# formula of area
+area = 3.14 * radius *radius
+
+# result
+print("the area of circle with radius " + str(radius) + "is " + str(area))
