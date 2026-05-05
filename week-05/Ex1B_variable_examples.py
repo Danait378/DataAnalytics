45c2343e57af028405883569ab16d4a171381e2d
Danait378 <danfkr143@gmail.com>
2026-05-05 09:51:52 -0700

    Ex1B_variable_examples.py

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,37 @@
+# customer information
+Customer_id = "001" # i used number because ids are numbers
+customer_name = "Anna Hernandez" # i put first and last name together.
+customer_gender = "Female" # i used text because gender is written as words.
+customer_date_of_birth = "03/14/1994" # i use month/day/year format.
+driver_license_number = "A3478697" # i think license numbers can have letters and numbers.
+auto_policy_number = "Auto8765435 " # i made up a policy number with letters and numbers.
+
+
+# my information
+my_name = "Danait berhane" # i used my name and last name
+city_state_grew_up = "charlotte, north carolina" # i added both city and state.
+
+
+# a) full list of reserved words that can't be used for variable names.
+
+# False, None, True, not, class, break, for, form, or
+# def, del, if, raise, import, return, and, else
+# while, is, as, except, assert, with, continue, global, pass, 
+# local, lampda, finally
+
+# b) 5 explanation of the reserved words
+
+# if → used to check if a condition is true.
+
+# else → runs when the if condition is false.
+
+# for → used to repeat code in a loop.
+
+# import → used to bring in Python libraries or files.
+
+# break → used to stop a loop before it finishes.
+
+
+
+
+
