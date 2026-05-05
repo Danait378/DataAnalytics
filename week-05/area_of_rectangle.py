a60c9191aebaf2714480c5111b615e2409cc70eb
Danait378 <danfkr143@gmail.com>
2026-05-05 13:59:18 -0700

    area_of_rectangle.py

---
+++ week-05/area_of_rectangle.py
@@ -0,0 +1,11 @@
+# Birthday rectangle sides
+month = 11
+day = 20
+
+# Multiply by the sides 
+rectangle_area = month * day
+
+# results
+print("side A is " + str(month))
+print("side B is" + str(day))
+print("The area of the rectangle is" + str(rectangle_area))
