041f214131d063fac1334eb38e4789ef5939621d
Danait378 <danfkr143@gmail.com>
2026-05-05 12:27:34 -0700

    net_worth.py

---
+++ week-05/net_worth.py
@@ -0,0 +1,22 @@
+# List of Assets
+checking_account = 2000
+saving_account = 8000
+phone_value = 1500
+
+# list of Debts
+credit_card = 1000
+phone_debt= 500
+
+# Totals
+
+total_assets = checking_account + saving_account + phone_value
+
+total_debts = credit_card + phone_debt
+
+# Net worth
+Net_worth = total_assets - total_debts
+
+# display out put
+print("total assets are " + str(total_assets))
+print("total debt are"+ str(total_debts))
+print("Net worth is " + str(Net_worth))
