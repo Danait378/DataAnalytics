c4360fb09635f8cd9ee6498954d39b1a2ac3795d
Danait378 <danfkr143@gmail.com>
2026-05-05 19:54:35 -0700

    rule_of_72.py

--- week-05/rule_of_72.py
+++ week-05/rule_of_72.py
@@ -1,16 +1,20 @@
-# my savings amount
-saving = 8000
+# Ask user for saving account
+savings = float(input("what is your savings amount"))
 
-# interest rate
-interest_rate = 0.05
+# Ask user for interest rate
+interest_rate = float(input("what is the interest rate"))
 
 # rule of 72
-year = 72 /(interest_rate * 100)
+year = 72 / interest_rate
 
 # Double savings
-Double_saving = saving * 2
+new_amount = savings * 2
 
 # result
-print("current saving is $" + str(saving))
+print("current saving is $" + str(savings))
 print(" At a " + format(interest_rate, ".0%") + " interest rate, saving account")
-print("worth $" + format(Double_saving, ".2f") + " in " + format(year, ".1f") + " years")
+print("worth $" + format(new_amount) + " in " + str(year) + "years")
+
+# observations
+# input() reads values as text (strings)
+# float() is for math calculation
